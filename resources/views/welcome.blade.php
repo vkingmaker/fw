15fd3c04a5463a1ee505e81fcbd0f86d82e98b3d
Monday Victor A <vkingmaker1@gmail.com>
2020-10-11 21:16:56 +0100

    fix css relative path

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Findworka</title>
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="/css/app.css" rel="stylesheet">
 </head>
 
 <body>
